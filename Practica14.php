eae3d80c2486ab5d99f854e50462a671a3a33bd0
Indra <132957866+lumity205@users.noreply.github.com>
2026-05-18 17:42:49 -0600

    Practica 14
    Cree un formulario que permita ingresar una cantidad en metros y envíe el dato a
    otro archivo PHP donde se convierta y muestre la cantidad en centímetros.

---
+++ Practica14.php
@@ -0,0 +1,11 @@
+<?php
+$metros = $_POST['metros'];
+$centimetros = $metros * 100;
+echo "<br><br>";
+echo "<h1><center>";
+echo "La medida en centímetros es $centimetros";
+echo "</h1></center>";
+?> 
+<?php
+echo "<br><a href='index.html'>Regresar al index</a>";
+?>
