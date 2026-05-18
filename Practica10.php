5cec0c67116a85f852b0d22e2e1f5d87cabd0f3a
Indra <132957866+lumity205@users.noreply.github.com>
2026-05-18 17:37:57 -0600

    Practica 10
    Diseñe un formulario que solicite tres notas de un estudiante y envíe la información
    a otro archivo PHP donde se calcule y muestre el promedio.

---
+++ Practica10.php
@@ -0,0 +1,17 @@
+<?php
+$nota1 = $_POST['nota1'];
+$nota2 = $_POST['nota2'];
+$nota3 = $_POST['nota3'];
+$promedio = ($nota1 + $nota2 + $nota3) / 3;
+echo "<br><br>";
+echo "<h1><center>";
+if ($promedio >= 7) {
+    echo "El promedio es $promedio. Aprobado";
+} else {
+    echo "El promedio es $promedio. Reprobado.";
+}
+echo "</h1></center>";
+?>
+<?php
+echo "<br><a href='index.html'>Regresar al index</a>";
+?>
